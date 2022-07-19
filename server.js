c35f0d365d99268620a4b97bb510f35207cb2bb6
Diana Hoveyan <dianahoveyan@gmail.com>
2022-07-19 18:02:12 +0400

    get request

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/build/index.html`);
 });
+app.get("/url", (req, res, next) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+});
 app.listen(9090, () => {
     console.log('Application listening on port 9090!');
 });
